d98ea71443054292f92b6f7b4eb942e9ecf69955
t-reynolds <timothy.reynolds@coloradocollege.edu>
2016-02-09 16:17:55 -0700

    Create Client.java

---
+++ app/src/main/java/com/mobilecomp/taylordelehanty/droneflight/Client.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Client extends Activity {
+    private Socket socket;
+    private static final int SERVERPORT = 4444;
+    private static final String SERVER_IP = "192.168.56.1";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(new ClientThread()).start();
+    }
+
+    public void onClick(View view) {
+        try {
+            EditText et = (EditText) findViewById(R.id.EditText01);
+            String str = et.getText().toString();
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),
+                    true);
+            out.println(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ClientThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+                socket = new Socket(serverAddr, SERVERPORT);
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
+    }
+}
