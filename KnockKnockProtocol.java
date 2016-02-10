f6a41d3cfa2ddf88a256293017cfc0fb00fd44d4
t-reynolds <timothy.reynolds@coloradocollege.edu>
2016-02-10 10:36:14 -0700

    Commits to js file

--- KnockKnockProtocol.java
+++ KnockKnockProtocol.java
@@ -2,10 +2,29 @@ import java.net.*;
 import java.io.*;
 
 public class KnockKnockProtocol {
-    public void processInput(String theInput) {
+    public void clearFile(){
+       try{
         PrintWriter writer = new PrintWriter("commands.js", "UTF-8");
-        writer.print(theInput);
-        writer.close();
+        writer.println("");
+        } 
+         catch(FileNotFoundException e){
+        System.out.println(e);
+       }
+     catch(UnsupportedEncodingException e){}
+
+    }
+    public void processInput(String theInput) {
+        try{
+             PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("commands.js", true)));
+             System.out.println(theInput);
+            out.println(theInput);
+            out.close();
+        }
+       catch(FileNotFoundException e){
+        System.out.println(e);
+       }
+       catch(UnsupportedEncodingException e){}
+       catch(IOException e){}
         
     }
 }
